b24a65150d9c6f2719bdf3f4d4a4ac20fdf2bac4
shanik hernandez <shaniik.her93@gmail.com>
2020-05-22 09:03:43 -0400

    made api file

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose")
+const logger = require("morgan")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const routes = require("./routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(logger("dev"));
 
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
-// routes
-app.use(require("./routes"));
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log("app on port ${PORT}");
